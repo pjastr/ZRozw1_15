940a3dd579c47c2be9721424c50019a6033ad93c
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:55:20 +0200

    Add files via upload

---
+++ ZRozw1_15/ZRozw1_15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tablica = new int[5, 5];
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    tablica[i, j] = 3 * j + 4 * i - 23;
+                }
+            }
+            Console.WriteLine("Pierwszy podpunkt");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i%2==0 && j%2==0)
+                    {
+                        Console.Write(tablica[i, j]+ " ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Drugi podpunkt");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i != j)
+                    {
+                        Console.Write(tablica[i, j] + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
